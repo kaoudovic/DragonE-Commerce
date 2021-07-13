2950bafb2f4355aee7e5e280f52b4449b8308349
azeem98 <ahmedazeem9889@gmail.com>
2021-07-13 09:44:09 -0700

    mmm

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use Searchable;
+    //use Searchable;
 
     protected $fillable = ['quantity'];
 
--- app/Product.php
+++ app/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use Searchable;
+    //use Searchable;
 
     protected $fillable = ['quantity'];
 
